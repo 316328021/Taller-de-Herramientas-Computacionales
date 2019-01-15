48cef1266621478c30273938d69a910e011a03dc
316328021 <brendagr00@ciencias.unam.mx>
2019-01-15 14:04:18 -0600

    ClasClase 055

--- Programas/funcion01.py
+++ Programas/funcion01.py
@@ -3,7 +3,27 @@ def vAbsoluto(x):
 		return(x)
 	else:
 		return(-x)
-x=10	
-while x>0:
-        x=x-1
-        print x
+def raiz(x):
+        h=x
+        b=1.0
+        e=0.0001
+        while vAbsoluto(b-h)>e:
+                h = (b+h)/2
+                b = x/h
+        return (b) #Se puede cualquiera de los dos, b o h, ya que son similares
+def raiz1(x):
+        h=x
+        b=1.0
+        e=0.0001
+        while vAbsoluto(b-h)>e:
+                h = (b+h)/2
+                b = x/h
+                print b
+        return (b)
+
+print raiz(1)
+print raiz(4)
+print raiz(9)
+print raiz(9.1)
+print raiz(1000000)
+
